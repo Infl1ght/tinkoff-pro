137a163ecbc96e800b90b85f07f8d0e6dd3d0410
Ilya Volkov <ilya.inflight@gmail.com>
2021-04-21 00:08:07 +0300

    Первая версия мега-калькулятора!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <v-main>
-      <MainComponent/>
+      <MainComponent />
     </v-main>
   </v-app>
 </template>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,27 +1,13 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
+        <h1 class="display-1 font-weight-bold mb-2 mt-5">
+          Выгодно ли Вам подключать Tinkoff Pro?
         </h1>
 
         <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a
-            href="https://community.vuetifyjs.com"
-            target="_blank"
-          >Discord Community</a>
+          Калькулятор поможет посчитать, подключать ли Вам Tinkoff Pro или нет
         </p>
       </v-col>
 
@@ -29,62 +15,22 @@
         class="mb-5"
         cols="12"
       >
+        <v-text-field
+          v-model="interestSum"
+          type="number"
+          min="0"
+          step="0.1"
+          label="Сумма на счете"
+          maxlength="20"
+        >
+        </v-text-field>
+        <h3 v-if="result">Выгодно!</h3>
+        <h3 v-else>Не выгодно!</h3>
         <h2 class="headline font-weight-bold mb-3">
           What's next?
         </h2>
 
         <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
         </v-row>
       </v-col>
     </v-row>
@@ -95,59 +41,24 @@
 import Vue from 'vue';
 
 export default Vue.extend({
-  name: 'HelloWorld',
+  name: 'MainComponent',
 
   data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/getting-started/pre-made-layouts',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
+    interestSum: '100',
   }),
+
+  watch: {
+    interestSum: (value) => {
+      console.log('value :>> ', value);
+    },
+  },
+
+  computed: {
+    result() {
+      const limit = (140 * 12) / 0.015;
+      const sum = parseFloat(this.interestSum);
+      return sum > limit;
+    },
+  },
 });
 </script>
