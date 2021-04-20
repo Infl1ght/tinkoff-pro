b17a09c5e22f1654f9a7e98e986d6c9d838a1636
Ilya Volkov <ilya.inflight@gmail.com>
2021-04-20 23:32:12 +0300

    Первый коммит

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app>
+    <v-main>
+      <MainComponent/>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import MainComponent from './components/MainComponent.vue';
+
+export default Vue.extend({
+  name: 'App',
+
+  components: {
+    MainComponent,
+  },
+
+  data: () => ({
+    //
+  }),
+});
+</script>
