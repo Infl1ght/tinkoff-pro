1a27fff5d7bfb5b49c41ba7d318d18322a6cfb32
Ilya Volkov <ilya.inflight@gmail.com>
2021-04-22 23:01:17 +0300

    Убрал Vuetify и изменил базовый шрифт на Rubik

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,40 +1,21 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col class="mb-4">
-        <h1 class="display-1 font-weight-bold mb-2 mt-5">
-          Выгодно ли Вам подключать Tinkoff Pro?
-        </h1>
+    <div>
+      <h1>
+        Выгодно ли Вам подключать Tinkoff Pro?
+      </h1>
 
-        <p class="subheading font-weight-regular">
-          Калькулятор поможет посчитать, подключать ли Вам Tinkoff Pro или нет
-        </p>
-      </v-col>
-
-      <v-col
-        class="mb-5"
-        cols="12"
-      >
-        <v-text-field
-          v-model="interestSum"
-          type="number"
-          min="0"
-          step="0.1"
-          label="Сумма на счете"
-          maxlength="20"
-        >
-        </v-text-field>
-        <h3 v-if="result">Выгодно!</h3>
-        <h3 v-else>Не выгодно!</h3>
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
-
-        <v-row justify="center">
-        </v-row>
-      </v-col>
-    </v-row>
-  </v-container>
+      <p class="subheading font-weight-regular">
+        Простой калькулятор поможет посчитать, подключать ли Вам Tinkoff Pro или нет
+      </p>
+      <input
+        v-model="interestSum"
+        value
+        type="number"
+        min="0"
+        step="0.1" />
+      <h3 v-if="result">Выгодно!</h3>
+      <h3 v-else>Не выгодно!</h3>
+    </div>
 </template>
 
 <script lang="ts">
@@ -46,12 +27,6 @@ export default Vue.extend({
   data: () => ({
     interestSum: '100',
   }),
-
-  watch: {
-    interestSum: (value) => {
-      console.log('value :>> ', value);
-    },
-  },
 
   computed: {
     result() {
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,9 @@
 import Vue from 'vue';
 import App from './App.vue';
-import vuetify from './plugins/vuetify';
+import './css/main.css';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  vuetify,
   render: (h) => h(App),
 }).$mount('#app');
