533dfe462ebe739a6e95ee8524413188f2613886
Ilya Volkov <ilya.inflight@gmail.com>
2021-04-25 14:23:33 +0300

    Улучшение верстки

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -2,74 +2,83 @@
   <div class="centered">
     <h1>
       Выгодно ли Вам подключать Tinkoff Pro?
+      <img class="centered-image" src="@/assets/icons/thinking_face.svg" height="48"/>
     </h1>
     <span class="repo-link-block">
       <a class="repo-link" href="https://github.com/Infl1ght/tinkoff_pro_profit/">
-        <span class="centered-vertical">Репозиторий проекта на GitHub</span>
-        <img src="@/assets/icons/github.png" height="24"/>
+        <span>Репозиторий проекта на GitHub</span>
+        <img class="github-image" src="@/assets/icons/github.png" height="24"/>
       </a>
     </span>
     <p>
       Простой калькулятор поможет посчитать, подключать ли Вам <a href="https://www.tinkoff.ru/pro/">Tinkoff Pro</a> или нет
     </p>
-    <div>
-      Сколько начисляется процентов на остаток в месяц?
+    <div class="calculation-block">
+      <div class="main-calculation-block">
+        <div>
+          Сколько начисляется процентов на остаток в месяц?
+        </div>
+        <div>
+          <input
+            class="input-money"
+            v-model="interestSum"
+            value
+            type="number"
+            min="0"
+            step="0.1" />
+          ₽
+        </div>
+        <div class="or">
+          или
+        </div>
+        <div>
+          Среднемесячный остаток на карте:
+        </div>
+        <div>
+          <input
+            class="input-money"
+            v-model.lazy="averageMonthlyAmount"
+            value
+            type="number"
+            min="0"
+            step="0.1" />
+          ₽
+        </div>
+      </div>
+      <div class="input-checkbox">
+        <input type="checkbox" id="checkbox" v-model="payForNotifications">
+        Пользуюсь оповещениями об операциях по карте ({{ notificationsCost }}₽/месяц)
+      </div>
+      <div class="input-checkbox">
+        <input type="checkbox" id="checkbox" v-model="payForService">
+        Плачу за обслуживание карты ({{ serviceCost }}₽/месяц)
+      </div>
+      <div class="input-checkbox">
+        <input type="checkbox" id="checkbox" v-model="payForTinkoffMobile">
+        Пользуюсь Tinkoff Mobile
+      </div>
+      <div v-show="payForTinkoffMobile">
+        <div>
+          Плачу за минуты Tinkoff Mobile:
+        </div>
+        <div>
+          <input
+            class="input-money"
+            v-model.lazy="tMobileCost"
+            value
+            type="number"
+            min="0"
+            step="1" />
+          ₽
+        </div>
+      </div>
     </div>
-    <div>
-      <input
-        class="input-money"
-        v-model="interestSum"
-        value
-        type="number"
-        min="0"
-        step="0.1" />
-      ₽
-    </div>
-    <div class="or">
-      или
-    </div>
-    <div>
-      Среднемесячный остаток на карте:
-    </div>
-    <div>
-      <input
-        class="input-money"
-        v-model.lazy="averageMonthlyAmount"
-        value
-        type="number"
-        min="0"
-        step="0.1" />
-      ₽
-    </div>
-    <div>
-      <input type="checkbox" id="checkbox" v-model="payForNotifications">
-      Пользуюсь оповещениями об операциях по карте ({{ notificationsCost }}₽/месяц)
-    </div>
-    <div>
-      <input type="checkbox" id="checkbox" v-model="payForService">
-      Плачу за обслуживание карты ({{ serviceCost }}₽/месяц)
-    </div>
-    <div>
-      <input type="checkbox" id="checkbox" v-model="payForTinkoffMobile">
-      Пользуюсь Tinkoff Mobile
-    </div>
-    <div v-show="payForTinkoffMobile">
-    <div>
-      Плачу за минуты Tinkoff Mobile:
-    </div>
-    <div>
-      <input
-        class="input-money"
-        v-model.lazy="tMobileCost"
-        value
-        type="number"
-        min="0"
-        step="1" />
-      ₽
-    </div>
-    </div>
-    <h3 v-if="result">Выгодно!</h3>
-    <h3 v-else>Не выгодно!</h3>
+
+    <h3>
+      Вердикт:
+      <span v-if="result">выгодно!</span>
+      <span v-else>не выгодно!</span>
+    </h3>
     <h4>За месяц вы будете дополнительно получать {{ round(balanceWithPro - balanceWithoutPro) }}₽</h4>
     Как посчиталась эта сумма?
     За счёт повышенного процента на остаток по счёту
@@ -147,7 +156,6 @@ export default {
       if (this.payForTinkoffMobile) {
         additionalCost += parseFloat(this.tMobileCost);
       }
-      console.log('additionalCost :>> ', additionalCost);
       return additionalCost;
     },
     balanceWithPro() {
@@ -157,7 +165,8 @@ export default {
       return this.incomeWithoutPro - this.additionalCostsWithoutPro;
     },
     result() {
-      return this.balanceWithPro > 0;
+      console.log('additionalCost :>> ', this.balanceWithPro, this.balanceWithoutPro);
+      return this.balanceWithPro > this.balanceWithoutPro;
     },
   },
 }
@@ -165,7 +174,7 @@ export default {
 
 <style scoped>
   h1 {
-    margin-bottom: 10px;
+    margin-bottom: 0px;
   }
   a {
     text-decoration: none;
@@ -176,11 +185,13 @@ export default {
     color: #000;
   }
   p {
-    text-indent: 15px;
+    margin-block-start: 0.25em;
   }
 
   .input-money {
-    width: 100px;
+    width: 120px;
+    font-size: 1.15rem;
+    margin: 5px 0px;
   }
   .centered {
     margin: 0 auto;
@@ -204,6 +215,26 @@ export default {
     font-size: 15px;
   }
   .or {
-    margin-left: 30px;
+    margin: 5px 20px;
+    font-size: 1rem;
+  }
+  .calculation-block {
+    background: rgba(0, 0, 0, .07);
+    border-radius: 8px;
+    padding: 15px;
+    font-size: 1.15rem;
+  }
+  .main-calculation-block {
+    margin-bottom: 15px;
+  }
+  .centered-image {
+    vertical-align: middle;
+  }
+  .github-image {
+    vertical-align: middle;
+    margin-left: 4px;
+  }
+  .input-checkbox {
+    margin: 10px 0px;
   }
 </style>
