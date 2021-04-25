b764f37affe48e883817306c2ffc5c2f4c75c473
Ilya Volkov <ilya.inflight@gmail.com>
2021-04-25 23:57:34 +0300

    Добавлены тултипы

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,44 @@
+<template>
+  <span class="tooltip">
+    <span class="data"><slot /></span>
+    <img class="centered-image" src="@/assets/icons/question_mark.svg" height="20" />
+  </span>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+  .tooltip {
+    position: relative;
+    cursor: pointer;
+  }
+
+  .tooltip .data {
+    position: absolute;
+    top: 0;
+    left: 0.5rem;
+    background: #041a2c; 
+    color: #fff; 
+    width: 250px;
+    font-size: 0.9rem;
+    padding: 0.8em;
+    box-shadow: 2px 2px 5px rgba(0, 0, 0, 0.3); /* Параметры тени */
+    pointer-events: none; /* Подсказка */
+    opacity: 0; /* Подсказка невидима */
+    transition: 0.5s; /* Время появления подсказки */
+    border-radius: 8px;
+    z-index: 2;
+  }
+
+  .tooltip:hover .data {
+    opacity: 0.9;
+  }
+
+  .data:hover {
+    opacity: 0.9;
+  }
+</style>
