e5d232ca5c26fb47209dfddbe1d1a5287a6a1153
Ilya Volkov <ilya.inflight@gmail.com>
2021-04-25 17:53:23 +0300

    Улучшена верстка

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -11,7 +11,7 @@
       </a>
     </span>
     <p>
-      Простой калькулятор поможет посчитать, подключать ли Вам <a href="https://www.tinkoff.ru/pro/">Tinkoff Pro</a> или нет
+      Простой калькулятор поможет посчитать, подключать ли Вам <a class="underlined" href="https://www.tinkoff.ru/pro/">Tinkoff Pro</a> или нет
     </p>
     <div class="calculation-block">
       <div class="main-calculation-block">
@@ -56,34 +56,55 @@
       <div class="input-checkbox">
         <input type="checkbox" id="checkbox" v-model="payForTinkoffMobile">
         Пользуюсь Tinkoff Mobile
+      <span v-show="payForTinkoffMobile">
+        и плачу за минуты Tinkoff Mobile
+        <input
+          class="input-money-small"
+          v-model.lazy="tinkoffMobileCost"
+          value
+          type="number"
+          min="0"
+          step="1" />
+        ₽
+      </span>
       </div>
-      <div v-show="payForTinkoffMobile">
-        <div>
-          Плачу за минуты Tinkoff Mobile:
-        </div>
-        <div>
-          <input
-            class="input-money"
-            v-model.lazy="tMobileCost"
-            value
-            type="number"
-            min="0"
-            step="1" />
-          ₽
-        </div>
+      <div>
+        Оцениваю дополнительные бонусы от Tinkoff Pro в
+        <input
+          class="input-money-small"
+          v-model.lazy="additionalProfit"
+          value
+          type="number"
+          min="0"
+          step="1" />
+        ₽
       </div>
     </div>
 
     <h3>
       Вердикт:
-      <span v-if="result">выгодно!</span>
-      <span v-else>не выгодно!</span>
+      <span v-if="result" class="profit">выгодно!</span>
+      <span v-else class="loss">не выгодно!</span>
     </h3>
-    <h4>За месяц вы будете дополнительно получать {{ round(balanceWithPro - balanceWithoutPro) }}₽</h4>
-    Как посчиталась эта сумма?
-    За счёт повышенного процента на остаток по счёту
-    ({{ round(interestPro * 100)}}% вместо {{ round(interest * 100) }}%) вы получите {{ round(incomeWithPro) }}₽ вместо {{ round(incomeWithoutPro) }}₽, 
-    но заплатите {{ proCost }}₽ за Tinkoff Pro
+    <h4>
+      В месяц вы будете
+      <span v-if="result" class="profit">дополнительно получать <span class="result-sum">{{ round(balanceWithPro - balanceWithoutPro) }}₽</span></span>
+      <span v-else class="loss">дополнительно платить <span class="result-sum">{{ round(balanceWithoutPro - balanceWithPro) }}₽</span></span>
+    </h4>
+    <span class="underlined cursor-pointer" @click="showCalculationInfo = !showCalculationInfo">Как посчиталась эта сумма?</span>
+    <div v-show="showCalculationInfo">
+      <table>
+        <tbody>
+          <tr><td>Дополнительная прибыль с увеличенного процента на остаток (+{{round((interestPro - interest) * 100)}}%)</td><td>+{{ round(interestIncomeWithPro - interestIncomeWithoutPro) }}₽</td></tr>
+          <tr v-if="payForNotifications"><td>Экономия на плате за оповещения</td><td>+{{ notificationsCost }}₽</td></tr>
+          <tr v-if="payForService"><td>Экономия на плате за обслуживание</td><td>+{{ serviceCost }}₽</td></tr>
+          <tr v-if="payForTinkoffMobile"><td>Экономия на плате за Tinkoff Mobile</td><td>+{{ tinkoffMobileCost }}₽</td></tr>
+          <tr v-if="parseFloat(additionalProfit)"><td>Дополнительные бонусы от Tinkoff Pro</td><td>+{{ additionalProfit }}₽</td></tr>
+          <tr><td>Стоимость Tinkoff Pro</td><td>-{{ proCost }}₽</td></tr>
+          <tr class="abovelined"><td>Итог</td><td>{{ round(balanceWithPro - balanceWithoutPro) }}₽</td></tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
@@ -92,8 +113,8 @@ export default {
   name: 'MainComponent',
   data() {
     return {
-      interestSum: 100,
-      averageMonthlyAmount: 20000,
+      interestSum: 175,
+      averageMonthlyAmount: 60000,
       payForNotifications: true,
       payForTinkoffMobile: false,
       payForService: false,
@@ -103,7 +124,9 @@ export default {
       notificationsCost: 59,
       proCost: 199,
       serviceCost: 99,
-      tMobileCost: 149,
+      tinkoffMobileCost: 149,
+      showCalculationInfo: false,
+      additionalProfit: 0,
     }
   },
   mounted() {
@@ -135,12 +158,15 @@ export default {
     interestMonthlyPro() {
       return this.interestPro / 12;
     },
-    incomeWithPro() {
+    interestIncomeWithPro() {
       const amountForInterest = this.averageMonthlyAmount < this.maxAmountForInterest
         ? this.averageMonthlyAmount : this.maxAmountForInterest;
       return amountForInterest * this.interestMonthlyPro;
     },
-    incomeWithoutPro() {
+    incomeWithPro() {
+      return this.interestIncomeWithPro + parseFloat(this.additionalProfit);
+    },
+    interestIncomeWithoutPro() {
       const amountForInterest = this.averageMonthlyAmount < this.maxAmountForInterest
         ? this.averageMonthlyAmount : this.maxAmountForInterest;
       return amountForInterest * this.interestMonthly;
@@ -148,13 +174,13 @@ export default {
     additionalCostsWithoutPro() {
       let additionalCost = 0;
       if (this.payForNotifications) {
-        additionalCost += this.notificationsCost;
+        additionalCost += parseFloat(this.notificationsCost);
       }
       if (this.payForService) {
-        additionalCost += this.serviceCost;
+        additionalCost += parseFloat(this.serviceCost);
       }
       if (this.payForTinkoffMobile) {
-        additionalCost += parseFloat(this.tMobileCost);
+        additionalCost += parseFloat(this.tinkoffMobileCost);
       }
       return additionalCost;
     },
@@ -162,7 +188,7 @@ export default {
       return this.incomeWithPro - this.proCost;
     },
     balanceWithoutPro() {
-      return this.incomeWithoutPro - this.additionalCostsWithoutPro;
+      return this.interestIncomeWithoutPro - this.additionalCostsWithoutPro;
     },
     result() {
       console.log('additionalCost :>> ', this.balanceWithPro, this.balanceWithoutPro);
@@ -176,10 +202,28 @@ export default {
   h1 {
     margin-bottom: 0px;
   }
-  a {
+  h3 {
+    margin-top: 7px;
+    margin-bottom: 0px;
+    font-size: 1.5rem;
+  }
+  h4 {
+    margin-top: 5px;
+    margin-bottom: 5px;
+  }
+  .loss {
+    color: red;
+  }
+  .profit {
+    color: green;
+  }
+  .underlined {
     text-decoration: none;
     border-bottom: dashed 2px;
     color: #000;
+  }
+  .cursor-pointer {
+    cursor: pointer;
   }
   a:visited {
     color: #000;
@@ -193,9 +237,14 @@ export default {
     font-size: 1.15rem;
     margin: 5px 0px;
   }
+  .input-money-small {
+    width: 60px;
+    font-size: 1rem;
+    margin-left: 5px;
+  }
   .centered {
     margin: 0 auto;
-    padding-top: 10px;
+    padding-top: 5px;
     max-width: 800px;
   }
   .repo-link-block {
@@ -213,6 +262,7 @@ export default {
   a.repo-link {
     border-bottom: none;
     font-size: 15px;
+    text-decoration: none;
   }
   .or {
     margin: 5px 20px;
@@ -222,7 +272,7 @@ export default {
     background: rgba(0, 0, 0, .07);
     border-radius: 8px;
     padding: 15px;
-    font-size: 1.15rem;
+    font-size: 1.1rem;
   }
   .main-calculation-block {
     margin-bottom: 15px;
@@ -236,5 +286,21 @@ export default {
   }
   .input-checkbox {
     margin: 10px 0px;
+  }
+  .result-sum {
+    font-size: 1.5rem;
+    color: black;
+  }
+  .abovelined {
+    border-top: 1px solid black;
+  }
+  table {
+    border-collapse: collapse;
+    padding: 5px;
+    margin-top: 5px;
+    background: rgba(0, 0, 0, .05);
+  }
+  td {
+    padding: 5px;
   }
 </style>
