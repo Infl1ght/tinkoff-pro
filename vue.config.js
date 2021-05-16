cc0f3ab05144eced452128c6673894d93b5800d5
Ilya Volkov <ilya.inflight@gmail.com>
2021-05-16 14:15:04 +0300

    Убрал webpack-bundle-analyzer

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,6 @@
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
-
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
     ? '/tinkoff-pro/'
     : '/',
   productionSourceMap: false,
-  configureWebpack: {
-    plugins: [new BundleAnalyzerPlugin()]
-}
 }
